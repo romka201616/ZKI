3585f7e08028a66b18beac4d3c95dfb3c00bbb11
romka201616 <mironovkilleer@gmail.com>
2024-03-01 23:41:30 +0300

    m

---
+++ ZKI_Lab1/Form9.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace ZKI_Lab1
+{
+    partial class Form9
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            richTextBox5 = new RichTextBox();
+            richTextBox6 = new RichTextBox();
+            richTextBox7 = new RichTextBox();
+            richTextBox8 = new RichTextBox();
+            richTextBox9 = new RichTextBox();
+            richTextBox10 = new RichTextBox();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            richTextBox11 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(10, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(223, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(25, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Закрытый ключ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(10, 74);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Взять всю последовательность";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(10, 116);
+            button2.Name = "button2";
+            button2.Size = new Size(223, 36);
+            button2.TabIndex = 3;
+            button2.Text = "Взять только первое число";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(10, 158);
+            button3.Name = "button3";
+            button3.Size = new Size(223, 36);
+            button3.TabIndex = 4;
+            button3.Text = "Взять из файла";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(25, 406);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Сообщение";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(10, 434);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(223, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(25, 218);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 25);
+            label3.TabIndex = 8;
+            label3.Text = "m";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(10, 246);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(223, 31);
+            textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(10, 367);
+            button4.Name = "button4";
+            button4.Size = new Size(223, 36);
+            button4.TabIndex = 11;
+            button4.Text = "Взять из файла";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(10, 325);
+            button5.Name = "button5";
+            button5.Size = new Size(223, 36);
+            button5.TabIndex = 10;
+            button5.Text = "Сгенерировать число";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(10, 283);
+            button6.Name = "button6";
+            button6.Size = new Size(223, 36);
+            button6.TabIndex = 9;
+            button6.Text = "Взять число";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 471);
+            button7.Name = "button7";
+            button7.Size = new Size(223, 36);
+            button7.TabIndex = 12;
+            button7.Text = "Зашифровать";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(331, 97);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(46, 235);
+            richTextBox1.TabIndex = 13;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(383, 97);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(46, 235);
+            richTextBox2.TabIndex = 14;
+            richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(435, 97);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(46, 235);
+            richTextBox3.TabIndex = 15;
+            richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(487, 97);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(46, 235);
+            richTextBox4.TabIndex = 16;
+            richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            richTextBox5.Location = new Point(539, 97);
+            richTextBox5.Name = "richTextBox5";
+            richTextBox5.Size = new Size(46, 235);
+            richTextBox5.TabIndex = 17;
+            richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Location = new Point(591, 97);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(46, 235);
+            richTextBox6.TabIndex = 18;
+            richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            richTextBox7.Location = new Point(643, 97);
+            richTextBox7.Name = "richTextBox7";
+            richTextBox7.Size = new Size(46, 235);
+            richTextBox7.TabIndex = 19;
+            richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            richTextBox8.Location = new Point(695, 97);
+            richTextBox8.Name = "richTextBox8";
+            richTextBox8.Size = new Size(46, 235);
+            richTextBox8.TabIndex = 20;
+            richTextBox8.Text = "";
+            // 
+            // richTextBox9
+            // 
+            richTextBox9.Location = new Point(747, 97);
+            richTextBox9.Name = "richTextBox9";
+            richTextBox9.Size = new Size(46, 235);
+            richTextBox9.TabIndex = 21;
+            richTextBox9.Text = "";
+            // 
+            // richTextBox10
+            // 
+            richTextBox10.Location = new Point(799, 97);
+            richTextBox10.Name = "richTextBox10";
+            richTextBox10.Size = new Size(46, 235);
+            richTextBox10.TabIndex = 22;
+            richTextBox10.Text = "";
+            // 
+            // button8
+            // 
+            button8.Location = new Point(258, 471);
+            button8.Name = "button8";
+            button8.Size = new Size(223, 36);
+            button8.TabIndex = 27;
+            button8.Text = "Взять из файла";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(258, 429);
+            button9.Name = "button9";
+            button9.Size = new Size(223, 36);
+            button9.TabIndex = 26;
+            button9.Text = "Сгенерировать число";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(258, 387);
+            button10.Name = "button10";
+            button10.Size = new Size(223, 36);
+            button10.TabIndex = 25;
+            button10.Text = "Взять число";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(273, 322);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 25);
+            label4.TabIndex = 24;
+            label4.Text = "n";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(258, 350);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(223, 31);
+            textBox4.TabIndex = 23;
+            // 
+            // richTextBox11
+            // 
+            richTextBox11.Location = new Point(851, 97);
+            richTextBox11.Name = "richTextBox11";
+            richTextBox11.Size = new Size(46, 235);
+            richTextBox11.TabIndex = 28;
+            richTextBox11.Text = "";
+            // 
+            // Form9
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(937, 564);
+            Controls.Add(richTextBox11);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(richTextBox10);
+            Controls.Add(richTextBox9);
+            Controls.Add(richTextBox8);
+            Controls.Add(richTextBox7);
+            Controls.Add(richTextBox6);
+            Controls.Add(richTextBox5);
+            Controls.Add(richTextBox4);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(button7);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form9";
+            Text = "Form9";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox4;
+        private RichTextBox richTextBox5;
+        private RichTextBox richTextBox6;
+        private RichTextBox richTextBox7;
+        private RichTextBox richTextBox8;
+        private RichTextBox richTextBox9;
+        private RichTextBox richTextBox10;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Label label4;
+        private TextBox textBox4;
+        private RichTextBox richTextBox11;
+    }
+}
---
+++ ZKI_Lab1/Form9.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZKI_Lab1
+{
+    public partial class Form9 : Form
+    {
+        public static List<int> Sequence { get; set; } = new List<int>();
+        public static string Message { get; set; } = string.Empty;
+        public static int M { get; set; }
+        public static int N { get; set; }
+
+        public Form9()
+        {
+            InitializeComponent();
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox1.DeselectAll();
+            richTextBox2.SelectAll();
+            richTextBox2.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox2.DeselectAll();
+            richTextBox3.SelectAll();
+            richTextBox3.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox3.DeselectAll();
+            richTextBox4.SelectAll();
+            richTextBox4.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox4.DeselectAll();
+            richTextBox5.SelectAll();
+            richTextBox5.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox5.DeselectAll();
+            richTextBox6.SelectAll();
+            richTextBox6.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox6.DeselectAll();
+            richTextBox7.SelectAll();
+            richTextBox7.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox7.DeselectAll();
+            richTextBox8.SelectAll();
+            richTextBox8.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox8.DeselectAll();
+            richTextBox9.SelectAll();
+            richTextBox9.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox9.DeselectAll();
+            richTextBox10.SelectAll();
+            richTextBox10.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox10.DeselectAll();
+            richTextBox11.SelectAll();
+            richTextBox11.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox11.DeselectAll();
+        }
+
+        private string ReadFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+            openFileDialog.Title = "Выберите файл";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filePath);
+                return fileContent;
+            }
+
+            return string.Empty;
+        }
+
+        private void SaveToFile(string text)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+            saveFileDialog.Title = "Сохранить файл";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                File.WriteAllText(filePath, text);
+                MessageBox.Show("Файл успешно сохранен.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] numberStrings = textBox1.Text.Split(' ');
+            var tmp = new List<int>();
+            bool ascendant = true;
+
+            foreach (string numberString in numberStrings)
+            {
+                int number = int.Parse(numberString);
+                tmp.Add(number);
+            }
+
+            for (int i = 0; i < tmp.Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum += tmp[j];
+                }
+                if (i > 0 && tmp[i] <= sum)
+                {
+                    ascendant = false;
+                    break;
+                }
+            }
+
+            if (tmp.Count == 8 && ascendant)
+            {
+                Sequence = tmp;
+                Message = textBox1.Text;
+                textBox1.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            Sequence.Add(number);
+            for (int i = 1; i < 8; i++)
+            {
+                number *= 2;
+                Sequence.Add(number);
+                textBox1.Text += " " + number.ToString();
+            }
+            textBox1.ForeColor = Color.Green;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = ReadFile();
+            string[] numberStrings = text.Split(' ');
+            var tmp = new List<int>();
+            bool ascendant = true;
+            textBox1.Clear();
+
+            foreach (string numberString in numberStrings)
+            {
+                int number = int.Parse(numberString);
+                tmp.Add(number);
+                textBox1.Text += number + " ";
+            }
+
+            for (int i = 0; i < tmp.Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum += tmp[j];
+                }
+                if (i > 0 && tmp[i] <= sum)
+                {
+                    ascendant = false;
+                    break;
+                }
+            }
+
+            if (tmp.Count == 8 && ascendant)
+            {
+                Sequence = tmp;
+                textBox1.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int tmp = int.Parse(textBox3.Text);
+            if (tmp > Sequence.Sum())
+            {
+                textBox3.ForeColor = Color.Green;
+                M = tmp;
+
+            }
+            else
+            {
+                textBox3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            M = Sequence.Sum() + 1;
+            textBox3.Text = M.ToString();
+            textBox3.ForeColor = Color.Green;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int tmp = int.Parse(ReadFile());
+            textBox3.Text = tmp.ToString();
+            if (tmp > Sequence.Sum())
+            {
+                textBox3.ForeColor = Color.Green;
+                M = tmp;
+
+            }
+            else
+            {
+                textBox3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Message = textBox1.Text.ToUpper();
+
+            for (int i = 0; i < Message.Length; i++)
+            {
+
+
+            }
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        
+
+        public static bool IsCoprime(int number1, int number2)
+        {
+            int gcd = FindGCD(number1, number2);
+
+            return gcd == 1;
+        }
+
+        public static int FindGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int tmp = int.Parse(textBox4.Text);
+            if (IsCoprime(tmp, M))
+            {
+                textBox4.ForeColor = Color.Green;
+                N = tmp;
+
+            }
+            else
+            {
+                textBox4.ForeColor = Color.Red;
+            }
+        }
+        public int GenerateCoprime(int number)
+        {
+            int coprime = 2;
+
+            while (true)
+            {
+                if (IsCoprime(number, coprime))
+                {
+                    textBox4.ForeColor = Color.Green;
+                    return coprime;
+                }
+
+                coprime++;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            N = GenerateCoprime(M);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int tmp = int.Parse(ReadFile());
+            textBox4.Text = tmp.ToString();
+            if (IsCoprime(tmp, M))
+            {
+                textBox4.ForeColor = Color.Green;
+                N = tmp;
+
+            }
+            else
+            {
+                textBox4.ForeColor = Color.Red;
+            }
+        }
+    }
+}
